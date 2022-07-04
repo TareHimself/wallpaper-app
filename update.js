a4074d298e27678d1a75f830fd42ef6467144567
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2022-07-04 11:27:58 -0400

    updated actions

--- update.js
+++ update.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-console */
 const { exec } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
 
+const PACKAGE_JSON_PATH = path.join(__dirname, 'release/app/package.json');
+const PACKAGE_LOCK_JSON_PATH = path.join(
+  __dirname,
+  'release/app/package-lock.json'
+);
 const version = process.argv[2] || '1.0.0';
+const packageJson = JSON.parse(readFileSync(PACKAGE_JSON_PATH));
+const packageLockJson = JSON.parse(readFileSync(PACKAGE_LOCK_JSON_PATH));
+
+packageJson.version = version;
+packageLockJson.version = version;
+
+writeFileSync(PACKAGE_JSON_PATH, JSON.stringify(packageJson, null, 2));
+writeFileSync(packageLockJson, JSON.stringify(PACKAGE_LOCK_JSON_PATH, null, 2));
+
 console.log(version);
 exec(
   `git add --all && git commit -m "test" && git tag v${version} && git push --tags`,
