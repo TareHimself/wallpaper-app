169be0d4b399ff35c3b0112b8509bdaedb3f4f74
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2022-07-04 11:05:19 -0400

    test

--- update.js
+++ update.js
@@ -1,7 +1,9 @@
 const { exec } = require('child_process');
 
+const version = process.argv[2] || '1.0.0';
+console.log(version);
 exec(
-  'git add --all && git commit -m "Test" && git tag test && git push --tags',
+  `git add --all && git commit -m "test" && git tag v${version} && git push --tags`,
   (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
